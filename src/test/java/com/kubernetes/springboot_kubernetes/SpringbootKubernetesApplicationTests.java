8ad9635996bd6463ff1e3b87b4931e210b7e9ae5
Nisha <alferniya.nisha@gmail.com>
2023-10-12 19:26:09 +0530

    first commit

---
+++ src/test/java/com/kubernetes/springboot_kubernetes/SpringbootKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.springboot_kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
